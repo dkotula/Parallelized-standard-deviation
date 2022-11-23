595c1fc627528281f039b781a36247c9fb6eb448
dkotula <dkotula.wieliczka@gmail.com>
2022-11-23 12:34:49 +0100

    Number generator and file with numbers created

--- numberGenerator.c
+++ numberGenerator.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    int i, n;
+
+    if (argc < 2)
+    {
+        n = 1000000;
+    }
+    else
+    {
+        n = atoi(argv[1]);
+    }
+
+    FILE *f = fopen("numbers.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
+    {
+        fprintf(f, "%d\n", rand() % 900000 + 100000);
+    }
+
+    fclose(f);
     return 0;
 }
